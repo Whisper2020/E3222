97a5f11eac9b60e1ea230cc57fadc81571e3a755
Snow02203835 <61280404+Snow02203835@users.noreply.github.com>
2020-03-02 23:32:58 +0800

    Create communication.py

---
+++ communication.py
@@ -0,0 +1,22 @@
+import time
+import serial
+import threading
+
+def sent(str):
+    sents = serial.Serial(str, 9600, timeout = 5)
+    print("串口=%s, 波特率=9600"%(sents.name))
+    strings = "[SENT {}]{}".format((time.asctime(time.localtime(time.time()))), str)
+    print(strings)
+    sents.write(strings.encode())
+    sents.close()
+def rece(str):
+    reces = serial.Serial(str, 9600, timeout = 5)
+    print("串口=%s, 波特率=9600"%(reces.name))
+    print("RECU from COM4: "+reces.readline().decode())
+    reces.close()
+
+if __name__ == '__main__':
+    t1 = threading.Thread(target=rece, args=("COM4",))
+    t2 = threading.Thread(target=sent, args=("COM3",))
+    t1.start()
+    t2.start()
